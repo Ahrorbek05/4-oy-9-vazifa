b87df42b5441cfc8ce5f4833652ef55e3e9d9e97
Ahrorbek05 <akramjonovahrorbek@gmail.com>
2024-06-05 10:30:39 +0500

    home work

---
+++ js/main.js
@@ -0,0 +1,90 @@
+// 1-masala
+// let arr = [];
+// arr.push (1, 2, 3);
+// arr.pop ([2]);
+// let m = arr.toString();
+// console.log(m);
+
+
+// 2-masala
+// let arr = [];
+// arr.unshift ( 1, 'salom', true, 4);
+// let s = arr.reverse();
+// console.log(s, arr.length);
+
+// 3-masala
+// let arr = [];
+// arr.unshift (5, 'bye', true, false);
+// arr.shift ([0]);
+// let a = arr.toString ();
+// console.log(a);
+
+// 4-masala
+// let arr  = [9, 19, 'xayr', true, 5, 99];
+// arr.pop ([5]);
+// let count = 0;
+// for ( i = 0; i > arr.length; i++){
+//     if ( count < arr.length){
+//         count++;
+//     }
+
+// }
+// console.log(count);
+
+// 5-masala
+// let arr = [100, 200, 300, 400,];
+// arr.push (500);
+// arr.at ([4]);
+// let n = arr.toString ([4]);
+// console.log(n);
+
+// 6-masala
+// let arr = ['salom', true, false, 5, 6];
+// arr.unshift ('xayr');
+// arr.reverse ();
+// let s = arr;
+// console.log(s,arr.length);
+
+// 7-masala
+// let arr = [1, 5, 7, 11, 12];
+// arr.shift ([0]);
+// let count = 0;
+// for ( i = 0; i <= count; i++){
+//     return count;
+// }
+// let a = arr.join()
+// arr.join (arr.toString());
+// console.log(a,count);
+
+// 8-masala
+// let arr =[1, 3, 5, 6, 8];
+// arr.push (9, 10);
+// arr.unshift (11);
+// for ( i = 0; i <= arr.length; i++){
+//     arr = arr.length;
+// }
+// arr.reverse ();
+// arr.toString ();
+// arr.join('');
+// console.log(arr.length);
+
+// 9-masala
+// let arr = [1, 5, 6, 9, 15, 100];
+// arr.pop ([5]);
+// arr.shift ([0]);
+// arr.reverse();
+// console.log(arr, arr.length);
+
+// 10-masala
+// let arr1 = [8, 9, 22, 63, 19];
+// let arr2 = [1, 5, 9, 6, 7];
+// let result = arr1.concat (arr2);
+// let result2 = arr1.slice([2])
+// let result3 = arr2.slice([2])
+// console.log(result, result2, result3, result.length);
+
+// 11-masala
+let arr = [1 ,2, 3, 4, 5, 6, 7];
+arr.slice ([3,6]);
+arr.reverse ();
+console.log(arr);
